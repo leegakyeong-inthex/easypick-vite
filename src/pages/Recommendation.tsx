dbcab235389c6d1ab7e7c73371c2d8440ade565c
leegakyeong-inthex <leegakyeong@inthe-x.com>
2025-12-30 14:59:05 +0900

    update recommendation page | add step 5

--- src/pages/Recommendation.tsx
+++ src/pages/Recommendation.tsx
@@ -33,6 +33,8 @@ import slateIcon from '@/assets/images/icons/slate.png';
 import theaterComedyIcon from '@/assets/images/icons/theater_comedy.png';
 import deleteIcon from '@/assets/images/icons/delete.png';
 import chevronRightIcon from '@/assets/images/icons/chevron_right.png';
+import nearMeIcon from '@/assets/images/icons/near_me.png';
+import { Link } from 'react-router';
 
 const places = [
   {
@@ -77,26 +79,36 @@ const ownedCards = [
     name: "신한카드 청춘대로 톡톡카드",
     benefit: "간편결제 10% 청구할인",
     image: card1,
+    spot: "스타벅스 뚝섬역점",
+    benefitDetail: "국내 1만원, 해외 5천원",
   },
   {
     name: "롯데카드 디지로카 Las Vegas",
     benefit: "국내외 모든 가맹점 최대 2%",
     image: card2,
+    spot: "블루보틀 뚝섬역점",
+    benefitDetail: "국내 1만원, 해외 5천원",
   },
   {
     name: "삼성카드 모니모니 카드",
     benefit: "포인트를 현금처럼 쓰는 카드",
     image: card3,
+    spot: "컴포즈커피 서울숲역점",
+    benefitDetail: "국내 1만원, 해외 5천원",
   },
   {
     name: "신한카드 청춘대로 톡톡카드",
     benefit: "국내외 모든 가맹점 최대 2%",
     image: card4,
+    spot: "블루보틀 서울숲점",
+    benefitDetail: "국내 1만원, 해외 5천원",
   },
   {
     name: "신한카드 청춘대로 톡톡카드",
     benefit: "국내외 가맹점 1.2% M포인트 적립",
     image: card5,
+    spot: "메가커피 왕십리점",
+    benefitDetail: "국내 1만원, 해외 5천원",
   },
 ]
 
@@ -140,12 +152,48 @@ const recommendedCards = [
   },
 ]
 
+const recommendedCardsWithCategory = [
+  {
+    SSM: [
+      {
+        name: "디지로카 Las Vegas",
+        benefit: "국내외 가맹점 최대 2% 할인!\n가맹점 2~3개얼 무이자 할부",
+        estimatedBenefit: "월 예상 혜택 17,453원",
+        image: card6,
+      },
+    ],
+  },
+  {
+    대형마트: [
+      {
+        name: "디지로카 Las Vegas",
+        benefit: "국내외 가맹점 최대 2% 할인!\n가맹점 2~3개얼 무이자 할부",
+        estimatedBenefit: "월 예상 혜택 17,453원",
+        image: card6,
+      },
+      {
+        name: "디지로카 Las Vegas",
+        benefit: "국내외 가맹점 최대 2% 할인!\n가맹점 2~3개얼 무이자 할부",
+        estimatedBenefit: "월 예상 혜택 17,453원",
+        image: card6,
+      },
+      {
+        name: "디지로카 Las Vegas",
+        benefit: "국내외 가맹점 최대 2% 할인!\n가맹점 2~3개얼 무이자 할부",
+        estimatedBenefit: "월 예상 혜택 17,453원",
+        image: card6,
+      },
+    ],
+  },
+]
+
 export default function Recommendation() {
   const [selectedPlace, setSelectedPlace] = useState<string>('전체');
   const [step, setStep] = useState<number>(1);
   const [selectedCategories, setSelectedCategories] = useState<Set<string>>(new Set());
   const [spendingAmounts, setSpendingAmounts] = useState<Record<string, string>>({});
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [doesSpendingExist, setDoesSpendingExist] = useState<boolean>(true);
 
   const toggleCategory = (categoryName: string) => {
     const newSelected = new Set(selectedCategories);
@@ -155,13 +203,6 @@ export default function Recommendation() {
       newSelected.add(categoryName);
     }
     setSelectedCategories(newSelected);
-  };
-
-  const handleAmountChange = (category: string, value: string) => {
-    setSpendingAmounts(prev => ({
-      ...prev,
-      [category]: value
-    }));
   };
 
   const handleKeypadInput = (value: string) => {
@@ -200,14 +241,37 @@ export default function Recommendation() {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col pb-20">
         <div className="font-bold text-xl mb-6 ml-[18px]">카드 추천</div>
         <div className="font-semibold text-lg mb-4 ml-[18px]">내 소비성향 맞춤 카드</div>
-        <div className="px-[18px] mb-3.5">
-          <img src={eventBanner2} width="339" height="124" className="w-full" />
-        </div>
+        {doesSpendingExist ? (
+          <>
+            <div className="mx-[18px] mb-2 flex items-center justify-between">
+              <div className="font-semibold text-[15px] text-[#6D727A]">총 {recommendedCards.length}개</div>
+              <div className="border border-[#EBEBEB] rounded-full h-[30px] px-[11px] py-[7px] text-[#5A5B64] font-semibold text-[13px]">월 소비금액 입력</div>
+            </div>
+            <div className="divide-y divide-solid divide-[#F4F4F4] px-[18px]">
+              {recommendedCards.map((card, i) => (
+                <div key={card.name+i} className="pt-[15px] pl-[12px] pb-[13px]">
+                  <div className="flex">
+                    <img src={card.image} width="67" height="102" alt={card.name} className="mr-4 object-contain" />
+                    <div className="flex-1">
+                      <div className="font-medium text-[#6D727A] text-xs mb-1.5">{card.name}</div>
+                      <div className="font-semibold text-sm whitespace-pre-line mb-[11px]">{card.benefit}</div>
+                      <div className="w-fit px-[9px] font-medium text-xs bg-[#D6E7FF] text-[#0068FF] rounded-full h-6 flex items-center justify-center">{card.estimatedBenefit}</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className="px-[18px] mb-3.5">
+            <img src={eventBanner2} width="339" height="124" className="w-full" />
+          </div>
+        )}
         <div className="px-[18px] mb-[30px]">
-          <Button className="font-medium text-sm" onClick={() => setStep(2)}>내 소비금액 입력하고 맞춤 카드 확인하기</Button>
+          <Button className="font-medium text-sm" onClick={() => setStep(doesSpendingExist ? 4 : 2)}>{doesSpendingExist ? '맞춤 카드 더보기' : '내 소비금액 입력하고 맞춤 카드 확인하기'}</Button>
         </div>
         <div className="font-semibold text-lg mb-[19px] ml-[18px]">혜택별 추천 카드</div>
         <div className="flex items-center space-x-4 overflow-x-scroll pb-[19px] px-[18px] border-b border-[#F4F4F4]">
@@ -230,8 +294,12 @@ export default function Recommendation() {
               <div key={card.name+i} className="flex items-center py-[8.5px] px-[10.5px]">
                 <img src={card.image} width="54" height="86" alt={card.name} className="mr-[12.5px]" />
                 <div className="flex-1">
-                  <div className="font-medium text-[#6D727A] text-[13px] mb-1">{card.name}</div>
-                  <div className="font-semibold text-base">{card.benefit}</div>
+                  <div className="flex items-center mb-1">
+                    <div className="font-medium text-[#6D727A] text-[13px]">{card.name}</div>
+                    {doesSpendingExist && <img src={chevronRightIcon} width="20" height="20" alt="더보기" />}
+                  </div>
+                  <div className="font-semibold text-base">{doesSpendingExist ? card.spot : card.benefit}</div>
+                  {doesSpendingExist && <div className="text-[13px] text-[#6D727A] mt-[3px]">{card.benefitDetail}</div>}
                 </div>
               </div>
               {selectedPlace !== '전체' && (
@@ -242,6 +310,11 @@ export default function Recommendation() {
             </div>
           ))}
         </div>
+        {doesSpendingExist && (
+          <div className="px-[18px] mt-3 mb-14">
+            <Button className="bg-[#F3F3F3] text-black" onClick={() => setStep(5)}>쇼핑 추천 카드 더보기</Button>
+          </div>
+        )}
       </div>
 
       {step === 2 && (
@@ -419,6 +492,65 @@ export default function Recommendation() {
                   <img src={chevronRightIcon} width="20" height="20" alt="더보기" />
                 </div>
               </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+      {step === 5 && (
+        <div className="absolute z-10 top-0 left-0 w-full h-full bg-white flex flex-col overflow-y-auto">
+          <div className="flex items-center px-[18px] mt-4 mb-5">
+            <img
+              src={arrowLeftIcon}
+              width="24"
+              height="24"
+              alt="뒤로가기"
+              className="object-contain"
+              onClick={() => {
+                setStep(1);
+              }}
+            />
+            <div className="font-bold text-xl ml-[18px]">쇼핑 추천 카드</div>
+          </div>
+          <div className="flex">
+            <div className="flex-1 h-[49px] flex items-center justify-center border-b-[2px] border-black font-semibold">신용카드</div>
+            <div className="flex-1 h-[49px] flex items-center justify-center border-b-[1px] border-[#F4F4F4] font-semibold text-[#75767A]">체크카드</div>
+          </div>
+          <div className="shrink-0 flex px-[18px] pt-4 overflow-x-scroll space-x-1.5">
+            {spendingCategories.map((category) => (
+              <div key={category.name} className="shrink-0 rounded-full bg-[#F7F8F8] h-9 px-3.5 text-[#5A5B64] text-sm flex items-center justify-center">{category.name}</div>
+            ))}
+          </div>
+          <div className="flex flex-col space-y-3 divide-y-[1px] divide-solid divide-[#F4F4F4]">
+            {recommendedCardsWithCategory.map((categoryGroup, i) => (
+              Object.entries(categoryGroup).map(([categoryName, cards]) => (
+                <div key={categoryName+i} className="mt-5 pb-6 px-[18px]">
+                  <div className="flex items-center justify-between mb-4">
+                    <div className="font-semibold text-lg">{categoryName}</div>
+                    <Link to="/search" className="flex items center space-x-[3px] px-2.5 py-[7px] rounded-full border border-[#EBEBEB]">
+                      <img src={nearMeIcon} width="16" height="16" alt="주변 찾기" />
+                      <div className="font-semibold text-[13px] text-[#5A5B64] leading-4">주변 찾기</div>
+                    </Link>
+                  </div>
+                  <div className="flex flex-col space-y-3">
+                    {cards.map((card, j) => (
+                      <div key={card.name+i} className="rounded-[10px] bg-[#F7F8F8] pt-[15px] pl-[12px]">
+                        <div className="flex">
+                          <img src={card.image} width="55" height="84" alt={card.name} className="mr-4 object-contain" />
+                          <div className="flex-1 flex flex-col justify-center">
+                            <div className="font-medium text-[#6D727A] text-xs mb-1.5">{card.name}</div>
+                            <div className="font-semibold text-sm whitespace-pre-line mb-[11px]">{card.benefit}</div>
+                          </div>
+                        </div>
+                        <div className="rounded-b-[10px] bg-[#F1F1F1] h-[42px] flex items-center justify-center font-semibold text-sm text-[#323232] mt-[15px]">
+                          <div>카드 신청</div>
+                          <img src={chevronRightIcon} width="20" height="20" alt="더보기" />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))
             ))}
           </div>
         </div>
